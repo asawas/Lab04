e77bc0c1cf08130d0e5abb8fde08a3c9024a9706
asawas <43736410+asawas@users.noreply.github.com>
2018-10-01 00:09:07 -0400

    add

---
+++ ledtester.c
@@ -0,0 +1,15 @@
+#include <wiringPi.h>
+int main (int argc, char *argv[])
+{
+  wiringPiSetup();
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  for (;;)
+  {
+    digitalWrite (0, HIGH) ; delay(1000);
+    digitalWrite (0, LOW) ; delay(500);
+    digitalWrite (1, HIGH) ; delay(1000);
+    digitalWrite (1, LOW) ; delay(500);
+  }
+  return 0;
+}
